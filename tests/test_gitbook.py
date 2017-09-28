5cf2c15beacffe7ce50df6cc8e16ea6561fa4278
matej <stavanja@gmail.com>
2017-09-28 22:15:49 +0200

    modify the test to check the length of code because it is important that at least one item be in the summary

--- tests/test_gitbook.py
+++ tests/test_gitbook.py
@@ -33,12 +33,12 @@ def test_create_list_of_files(set_book):
     set_book.book.copy_files_for_gitbook()
     set_book.book.create_a_list_of_files('poglavje-*/*')
     assert len(set_book.book.list_of_files) == len([item for item in os.listdir(set_book.gitbook) if set_book.folder in item])
-    
+
 
 def test_create_list_of_files_ugly_path(set_book):
     set_book.book.copy_files_for_gitbook()
     set_book.book.create_a_list_of_files('asdf-*/*')
-    
+
     assert len(set_book.book.list_of_files) == 0
 
 def test_internal_function_summary(set_book):
@@ -46,4 +46,4 @@ def test_internal_function_summary(set_book):
     set_book.book.copy_files_for_gitbook()
     book.create_a_list_of_files('poglavje-*/*')
     print(list(book._create_summary(book.list_of_files)))
-    assert len(list(book._create_summary(book.list_of_files))) > 0
+    assert len(list(book._create_summary(book.list_of_files))) > 1
