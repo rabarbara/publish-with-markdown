27b3d548ffc3191797658e0ff85ee3e5dd5b1708
matej <stavanja@gmail.com>
2017-09-30 15:01:30 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Borrowed from: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='publish_with_markdown',
+    version='0.1.0',
+    description='A collection of tools for a faster publishing of books with markdown',
+    long_description=readme,
+    author='Matej Stavanja',
+    author_email='matej@stavanja.io',
+    url='https://github.com/rabarbara/publish-with-markdown.git',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
