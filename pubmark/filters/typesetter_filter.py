3054a7659f843b54d3dd8731fc18901774b638c2
Matej Stavanja <matej.stavanja@zfm.si>
2018-05-10 12:53:00 +0200

    add typesetter filter that converts the file according to typesetter specs

---
+++ pubmark/filters/typesetter_filter.py
@@ -0,0 +1,26 @@
+"""
+Replace everything to accomodate the typesetters
+"""
+
+from panflute import *
+
+def action(elem, doc):
+    if isinstance(elem, Para) and isinstance(elem.next, BulletList):
+        return elem
+    if isinstance(elem, Para) and isinstance(elem.next, Header):
+        return elem
+    if isinstance(elem, Para) and elem.next == None:
+        pass
+    if isinstance(elem, Para):
+        e = Para(Str(''))
+        return [elem, e]
+    if isinstance(elem, Header):
+        e = Para(Str(''))
+        return [e, e, elem]
+        
+
+def main(doc=None):
+    return run_filter(action, doc=doc) 
+
+if __name__ == '__main__':
+    main()
